9211013279d8f485452825326cedb22019ab136c
Natanaelel <43886350+Natanaelel@users.noreply.github.com>
2021-06-19 16:27:19 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+let serverAdress = "1d7f4ca53b27.ngrok.io"
+// serverAdress = "drawline.herokuapp.com"
+const ws = new WebSocket("wss://" + serverAdress)
 
+ws.onmessage = message => {
+  msg = JSON.parse(message.data);
+
+  console.log(msg)
+  if(msg.method == "connect"){
+    const name = prompt("What do you want to be called?")
+    send("connect", name)
+  }
+  if(msg.method == "text"){
+    console.log(msg.data)
+  }
+  if(msg.method == "points"){
+    drawData(msg.data)
+  }
+  if(msg.method == "eval"){
+    eval(msg.data)
+  }
+
+}
+function send(method, data){
+  const payLoad = {
+    "method": method,
+    "data": data
+  }
+  if(ws.readyState==1){
+    ws.send(JSON.stringify(payLoad))
+  }else{
+    console.log("disconnected form server!")
+  }
+}
