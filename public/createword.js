c36a0390f2ce23eec5911ae011cff0880633513a
lolitzme <ldplayyy@gmail.com>
2021-06-19 17:10:43 +0200

    v0.000000001

---
+++ public/createword.js
@@ -0,0 +1,17 @@
+var randomword
+var word_length = 3
+function createWords(word_length = 2 ){
+    let randomword = ""
+    while(randomword.length <= word_length){
+        randomword = words[Math.floor(Math.random()*words.length)]
+    }
+    return randomword
+}
+function createChars(word_length = 2, randomword){
+    let randomchar
+    randomint = Math.floor(Math.random()*(randomword.length-word_length))
+    randomchar = randomword.slice(randomint,randomint+word_length)
+    return randomchar
+}
+randomword = createWords(word_length)
+randomchar = createChars(word_length, randomword)
