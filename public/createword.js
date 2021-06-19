f324746181f01afadf72d0d5ea65281e05c8f226
lolitzme <ldplayyy@gmail.com>
2021-06-19 22:57:13 +0200

    yessir

--- public/createword.js
+++ public/createword.js
@@ -1,6 +1,8 @@
 var randomword
 var randomchar
-var word_length = 3
+var word
+var player_guess
+var word_length = 2
 function createWords(word_length = 2 ){
     let randomword = ""
     while(randomword.length <= word_length){
@@ -14,5 +16,19 @@ function createChars(word_length = 2, randomword){
     randomchar = randomword.slice(randomint,randomint+word_length)
     return randomchar
 }
+function submit(player_guess){
+    console.log(player_guess)
+}
+
+input = document.getElementById("word")
+
+input.addEventListener("keyup",e=>{
+    if (e.keyCode == 13) {
+        e.preventDefault()
+        submit(input.value)
+        input.value = null
+    }
+})
+
 randomword = createWords(word_length)
 randomchar = createChars(word_length, randomword)
--- public/drawing.js
+++ public/drawing.js
@@ -1,6 +1,8 @@
 var heart
+var input
 function preload(){
-  heart = createImg('https://i.imgur.com/X81l5ZC.png')
+  heart = createImg('https://i.imgur.com/X81l5ZC.png','heart')
+  heart.hide()
 }
 
 function setup(){
@@ -13,15 +15,23 @@ function setup(){
 function draw(){
   fill("blue")
   background(200,200,200)
-  circle(width/2,height/2,(width*height)**(1/2)/5) 
-  circle(width/2,height-height/5,(width*height)**(1/2)/7)
+  circle(width/2,height/2,(width*height)**(1/2)/5)
+  fill("black")
+  textSize(128)
+  textAlign(CENTER,CENTER)
+  text(randomchar,width/2,height/2)
+  fill("blue")
 
-  image(heart, width/2, (height-height/5)+((width*height)**(1/2)/7), 50, 50)
+
 }
 
-
-function drawData(data){
-
+function drawPlayers(){
+  for(let i=0; i<players["players"].length; i++){ // draw Players
+    circle(width/2,height-height/5,(width*height)**(1/2)/7)
+    for(let i=0 i<players["hearts"] i++){ // draw Hearts
+      image(heart, width/2, (height-height/5)+((width*height)**(1/2)/7), 50, 50)
+    }
+  }
 }
 
 window.onresize = ()=>resizeCanvas(windowWidth, windowHeight)
