7db8c306752d0b0dc6389c8631cec28cba642e73
lolitzme <ldplayyy@gmail.com>
2021-06-19 20:49:42 +0200

    v0.00002

--- public/drawing.js
+++ public/drawing.js
@@ -1,57 +1,27 @@
+var heart
+function preload(){
+  heart = createImg('https://i.imgur.com/X81l5ZC.png')
+}
+
 function setup(){
-  canvas = createCanvas(windowWidth, windowHeight - 100)
+  canvas = createCanvas(windowWidth, windowHeight)
   canvas.parent('canvas')
   canvas.style("display","block")
+  imageMode(CENTER)
 }
-points = []
-drawColor = [255,0,0]
-size = 10
+
 function draw(){
-  // background(220)
-  // fill(255,0,0)
-  // circle(mouseX, mouseY, 30)
-  if(mouseIsPressed){
-    points.push([pmouseX, pmouseY])
-    stroke(drawColor)
-    strokeWeight(size)
-    line(pmouseX, pmouseY, mouseX, mouseY)
-  }else{
-    if(points.length > 0){
-      points.push([pmouseX, pmouseY])
-      send("points", {
-        points: points,
-        size: size,
-        color: drawColor
-      })
-    }
-    points = []
-  }
+  fill("blue")
+  background(200,200,200)
+  circle(width/2,height/2,(width*height)**(1/2)/5) 
+  circle(width/2,height-height/5,(width*height)**(1/2)/7)
+
+  image(heart, width/2, (height-height/5)+((width*height)**(1/2)/7), 50, 50)
 }
 
 
 function drawData(data){
-  prevpt = data.points[0]
-  for(pt of data.points.slice(1)){
-    strokeWeight(data.size)
-    stroke(data.color)
-    line(pt[0], pt[1], prevpt[0], prevpt[1])
-    prevpt = pt
-  }
 
 }
 
-
-document.getElementById("color-picker").addEventListener("change", element=>{
-  drawColor = colorFromHex(element.target.value)
-})
-document.getElementById("range").addEventListener("input", element=>{
-  size = element.target.value
-})
-
-function colorFromHex(hex){
-  console.log(hex)
-  let r = parseInt(hex.slice(1,3),16)
-  let g = parseInt(hex.slice(3,5),16)
-  let b = parseInt(hex.slice(5,7),16)
-  return([r, g, b])
-}
+window.onresize = ()=>resizeCanvas(windowWidth, windowHeight)
