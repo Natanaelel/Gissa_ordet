d17845d9fb9469507e7517baf7de2f49ac67e0ba
lolitzme <ldplayyy@gmail.com>
2021-06-23 01:18:49 +0200

    Added skin changer WIP

--- public/change_skin.js
+++ public/change_skin.js
@@ -1,30 +1,16 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+current_skin = 0
+// playerImg
 
-// Next/previous controls
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("demo");
-    var captionText = document.getElementById("caption");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+function plusDivs(arg=0){
+    current_skin += arg
+    current_skin %= playerImg.length
+    if (current_skin < 0) current_skin = playerImg.length-1
+    for(let i = 0; i < playerImg.length; i++){
+        if (i == current_skin){
+            getID(i).style.display = "block"
+        } else {
+            getID(i).style.display = "none"
+        }
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-    captionText.innerHTML = dots[slideIndex-1].alt;
+    console.log(current_skin, arg)
 }
